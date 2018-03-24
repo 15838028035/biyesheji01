8f0cdca51ee4f108d1c534a45e0b1fb975a6bc5d
15838028035 <15838028035@163.com>
2018-03-24 21:32:43 +0800

    #10 spring boot 集成swagger

--- backend/src/main/java/com/lj/config/WebMvcConfig.java
+++ backend/src/main/java/com/lj/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.lj.common.DateConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.concurrent.ExecutorService;
@@ -22,5 +23,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ExecutorService executorService() {
         return Executors.newCachedThreadPool();
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
